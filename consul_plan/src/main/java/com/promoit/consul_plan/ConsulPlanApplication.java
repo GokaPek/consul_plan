0ae6271d0708051011fe7aa8a2600359e1ecdc2c
GokaPek <Hgogogo@yandex.ru>
2024-09-21 20:41:06 +0400

    init

---
+++ consul_plan/src/main/java/com/promoit/consul_plan/ConsulPlanApplication.java
@@ -0,0 +1,13 @@
+package com.promoit.consul_plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulPlanApplication.class, args);
+	}
+
+}
---
+++ consul_plan/src/test/java/com/promoit/consul_plan/ConsulPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promoit.consul_plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
